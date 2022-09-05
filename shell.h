803e98c02077af1c78c23052a9c9178aef2230ca
David Adejumo <davidadejumo@gmail.com>
2022-09-05 12:13:29 -0700

    simple shell

--- shell.h
+++ shell.h
@@ -4,7 +4,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
+
+void ctrl_C(int);
+int _printf(char *, int);
+int _strlen(char *);
+char *_getline(void);
+
+#define READ_BUF 1000
 
 
 #endif
